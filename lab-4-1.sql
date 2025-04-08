6c4c23dd6dfab625eba2c742aef3d0ae569cffe6
Ryan Williams <ryan.williams@kellogg.northwestern.edu>
2025-04-08 16:30:22 +0000

    Latest changes at the end of class

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -6,4 +6,8 @@
 SELECT SUM(stats.hits)
 FROM stats 
 INNER JOIN players ON players.id = stats.player_id
-WHERE players.first_name = "Barry" AND players.last_name ="Bonds"
+WHERE players.first_name = "Barry" AND players.last_name ="Bonds";
+
+-- You need the where statements to tell SQL where to point for the player IDs in the other statements
+-- Remember that SQL mostly reads bottom up so this makes sense if the where is "First"
+-- Inner Join doesn't care about the order of the tables you pull from, some of the others do 
